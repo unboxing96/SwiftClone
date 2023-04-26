76edcffdc93ccc76ed20b62c8d54e9d5e73b1b5e
kimpepe <unboxing96@gmail.com>
2023-04-26 19:30:15 +0900

    ADD: proj

---
+++ week1/week1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  week1
+//
+//  Created by kimpepe on 2023/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ week1/week1/week1App.swift
@@ -0,0 +1,17 @@
+//
+//  week1App.swift
+//  week1
+//
+//  Created by kimpepe on 2023/04/26.
+//
+
+import SwiftUI
+
+@main
+struct week1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
