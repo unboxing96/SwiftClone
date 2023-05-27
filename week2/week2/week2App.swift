04b88fe4e12a3b10c71e0786f1f5480a3bc825fc
kimpepe <unboxing96@gmail.com>
2023-05-27 11:09:25 +0900

    NEW: week2

---
+++ week2/week2/week2App.swift
@@ -0,0 +1,17 @@
+//
+//  week2App.swift
+//  week2
+//
+//  Created by kimpepe on 2023/05/27.
+//
+
+import SwiftUI
+
+@main
+struct week2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
